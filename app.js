a60910b7d8893bb46b66af79a680068d09423e1a
devonhackley <devonhackley@gmail.com>
2016-11-04 14:20:03 -0700

    started the app.js file

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 'use strict';
+
+function convertUnits () {
+  var value = getElementById('measurement');
+  var unit  = getElementById('units');
+
+  if (unit === 'in-cm'){
+
+  } else if (unit === 'cm-in') {
+
+  } else if (unit === 'floz-mL'){
+
+  } else if (unit === 'mL-floz') {
+
+  } else if (unit === 'mi-km') {
+
+  } else if (unit === 'km-mi'){
+
+  } else if (unit === 'lbs-kg'){
+
+  } else if (unit === 'kg-lbs') {
+
+  } else if (unit === 'eshitton-mshitton') {
+
+  } else if (unit === 'mshitton-eshitton') {
+    
+  }
+}
+
+convertUnits();
